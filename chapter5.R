889c9b60877fe0bb7ebb229fcd23fb11170d7afe
Rios <1556544189157002@mil>
2023-06-08 15:49:52 -0400

    Updated files

---
+++ chapter5.R
@@ -0,0 +1,65 @@
+#### Chapter 5: Exploratory Data Analysis #####################################
+
+### Introduction ##############################################################
+
+# EDA: explore your data in a systematic way
+#
+# 1. Generate questions about your data
+# 2. Search for answers by visualizing, transforming, and modeling your data
+# 3. Use what you learn to refine your questions and generate new questions
+
+# Data cleaning is just one application of EDA: you ask questions about
+# whether or not your data meets your expectations. To do data cleaning,
+# you'll need to deploy all the tools of EDA: visualization, transformation,
+# and modeling.
+
+library(tidyverse)
+
+# Your goal during EDA is to develop an understanding of your data. The 
+# easiest way to do this is to use questions as tools to guide your 
+# investigation. The key to asking quality questions is to generate a
+# large quantity of questions. 
+
+# Two types of questions will always be useful for making discoveries within
+# your data:
+
+# 1. What type of variation occurs within my variables?
+# 2. What type of co-variation occurs between my variables?
+
+# A variable is a quantity, quality, or property that you can measure
+
+# A value is the state of a variable when you measure it
+
+# An observation (or data point) is a set of measurements made under similar 
+# conditions (you usually make all of the measurements in an observation
+# at the same time and on the same object). An observation will contain
+# several values, each associated with a different variable. 
+
+# Tabular data is a set of values, each associated with a variable and 
+# an observation. Tabular data is tidy if each value is placed in its
+# own cell, each variable in its own column, and each observation in 
+# its own row. 
+
+### Variation #################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
